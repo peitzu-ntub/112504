32525c7f9181271d5e3ac283938b079187c1f3a2
LydiaTW <lydia930113@gmail.com>
2023-05-04 16:57:19 +0800

    new

---
+++ Testing/Yuen/bin/menu_1.php
@@ -0,0 +1,28 @@
+<?php
+$user = 'root';
+$pass = '12345678';
+$db = '112504';
+//Replace 'sample tutorial' with the name of your database
+
+$con = mysqli_connect("localhost", $user, $pass, $db);
+
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
+
+}
+
+  $name = $_POST['type_name'];
+
+
+  $sql = "INSERT INTO food_type (type_name) 
+  VALUES ('$name')";
+	 
+    // header("Location: upload.html"); // Return to where our form is stored
+
+    //對資料庫執行查訪的動作
+    if (mysqli_query($con, $sql)) {
+      header("Location: upload.html");
+  }else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($con);
+    }
+?>
