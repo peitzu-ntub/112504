73e2cc803b174f3491b30f3e3cafaca205e7a942
YutungCheng <10856044@ntub.edu.tw>
2023-05-08 17:48:19 +0800

    new

--- demo2/page/newQrcode.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include "../qrcode/phpqrcode.php";
-    $param = isset($_GET['data']) ? urldecode($_GET['data']) : 'Test Sample.';
-    $codeText = $param;
-    QRcode::png($codeText);
-?>
