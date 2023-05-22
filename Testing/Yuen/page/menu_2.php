a60cb57afeec9a01f627a9cebbadf7c937fa2b39
LydiaTW <lydia930113@gmail.com>
2023-05-22 22:57:04 +0800

    change

--- Testing/Yuen/page/menu_2.php
+++ Testing/Yuen/page/menu_2.php
@@ -6,7 +6,7 @@ $db = '112504';
 $con = mysqli_connect("localhost", $user, $pass, $db);
 
   $sql = "SELECT type_name FROM food_type;";
-  $food_type = mysqli_query ($con, $sql);
+  $car_brands = mysqli_query ($con, $sql);
 
 ?>
 <!DOCTYPE html>
@@ -18,29 +18,29 @@ $con = mysqli_connect("localhost", $user, $pass, $db);
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>input food</title>
-	<link href="../js/menu.css" rel="stylesheet">
+	<link href="../js/test.css" rel="stylesheet">
 
 </head>
 
 
 <body>
 	<div class="container">
-		<div class="title">輸入所有餐點類型</div><br>
+		<div class="title">輸入所有餐點</div><br>
 		<hr>
-		
 		<div class="content">
-		<form action="../bin/2.php" method="POST"> 
+			<form action="../bin/menu2.php" method="POST">
 				<div class="user-details">
 					<div class="input-box">
-						<span class="details">餐點類型：</span>
+						<span class="details" >餐點類型：</span>
 						<select name="type_id" id="type_id">
+						<!-- 動態載入的選項會放在這裡 -->
 						<option value="">--- Select ---</option>
                         <?php
-                        while ($cat = mysqli_fetch_array($food_type,MYSQLI_ASSOC)) {
+                        while ($cat = mysqli_fetch_array($car_brands,MYSQLI_ASSOC)) {
 									echo "<option value='" . $cat['type_name'] . "'>" . $cat['type_name'] . "</option>";
 						}
                         ?>
-						</select>
+					</select>
 					</div>
 					<div class="input-box">
 						<span class="details">餐點名稱：</span>
@@ -59,27 +59,21 @@ $con = mysqli_connect("localhost", $user, $pass, $db);
 						<textarea id="meal_note" name="meal_note" rows="2" cols="20" placeholder="請輸入餐點介紹"
 							style="height: 50px;width: 300px;resize: none;"></textarea>
 					</div>
-					<div class="input-box">
-						<span class="details">注意事項：</span>
-						<!--textarea id="meal_note" name="meal_note" rows="2" cols="20" placeholder="注意事項"></textarea> -->
-						<textarea id='meal_note' name="meal_note" placeholder="請輸入注意事項"
-							style="height: 50px;width: 300px;resize: none;"></textarea>
-					</div>
 				</div>
 				<div class="button">
-					<input value="確定" type="submit" onclick= />
+					<input value="儲存" type="submit" onclick= />
 				</div>
 				<div class="button">
 					<input type="reset" value="返回" onclick="location.href='../page/menu_1.html'">
 				</div>
 			</form>
 			<div class="button-container">
-				<button class="registbutton" onclick="location.href='../page/menu_3.html'">
+				<button class="nextbutton" onclick="location.href='../page/menu_3.html'">
 					<span>下一步!</span>
 				</button>
 			</div>
 		</div>
+	</div>
 </body>
-
 
 </html>
