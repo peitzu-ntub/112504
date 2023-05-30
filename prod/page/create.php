e20ddb10db4dac8908130dab414426edbc0eb4d1
YutungCheng <10856044@ntub.edu.tw>
2023-05-30 23:17:30 +0800

    修改

--- prod/page/create.php
+++ prod/page/create.php
@@ -90,11 +90,7 @@
 					<input type="reset" value="返回" onclick="location.href='../page/position.php'">
 				</div>
 			</form>
-			<div class="button-container">
-				<button class="nextbutton" onclick="location.href='../page/menu_3.html'">
-					<span>下一步!</span>
-				</button>
-			</div>
+			
 		</div>
 	</div>
 </body>
