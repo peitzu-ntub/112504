91e23505ddd0e44333d1761778fa5529692641b4
YutungCheng <10856044@ntub.edu.tw>
2023-05-30 23:19:11 +0800

    修改返回

--- prod/page/create.php
+++ prod/page/create.php
@@ -87,7 +87,7 @@
 					<input value="儲存" type="submit"/>
 				</div>
 				<div class="button">
-					<input type="reset" value="返回" onclick="location.href='../page/position.php'">
+					<input type="reset" value="返回" onclick="location.href='../page/employee.html'">
 				</div>
 			</form>
 			
