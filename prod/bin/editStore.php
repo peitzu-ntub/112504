c7b0ed823ab2932e7c1c3be292fdc7025820174f
peitzu-ntub <10856021@ntub.edu.tw>
2023-08-11 17:05:02 +0800

    註冊店家測試

--- prod/bin/editStore.php
+++ prod/bin/editStore.php
@@ -4,7 +4,7 @@
     $data= array();
   
     try {
-        //$bossid = $_POST['lic'];
+        $bossid = $_POST['lic'];
         $storeid = $_POST['store_id'];
         $name = $_POST['store_name'];
         $tel = $_POST['store_tel'];
@@ -13,18 +13,18 @@
 
         //檢查店家是否已經存在
         //1.產生查詢字串
-        //$sql = "select * from store_info where store_id = '$id'";
+        $sql = "select * from store_info where store_id = '$id'";
         //2.查下去，並取得查詢結果
-        //$result = mysqli_query($con, $sql);
+        $result = mysqli_query($con, $sql);
         //3.查詢結果的筆數
-        //$count = mysqli_num_rows($result);
+        $count = mysqli_num_rows($result);
         //如果筆數大於0，表示這個店家已經存在
-        //if ($count > 0) {
-            //$data['result'] = 'NG';
-            //$data['message'] = "店家已經存在,請重新輸入";
-            //echo json_encode($data);
-            //exit();
-        //}
+        if ($count > 0) {
+            $data['result'] = 'NG';
+            $data['message'] = "店家已經存在,請重新輸入";
+            echo json_encode($data);
+            exit();
+        }
 
         //註冊店家資料
         //$sql = 
@@ -33,12 +33,11 @@
         //    ) VALUES (
         //        '$bossid', '$storeid', '$name', '$tel', '$address', '$table'
         //    )";
-
         $sql = 
             "INSERT INTO store_info (
-                store_id, store_name, store_tel, store_address, table_count
+                boss_identity, store_name, store_tel, store_address, table_count
             ) VALUES (
-                '$storeid', '$name', '$tel', '$address', '$table'
+                '$bossid', '$name', '$tel', '$address', '$table'
             )";
         if (mysqli_query($con, $sql)) {
             $data['result'] = 'OK';
