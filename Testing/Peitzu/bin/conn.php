8113d8e7ccc7a08af21c3f17bf9451f4b20481ca
peitzu-ntub <10856021@ntub.edu.tw>
2023-08-12 15:54:52 +0800

    資料庫連線問題解決

--- Testing/Peitzu/bin/conn.php
+++ Testing/Peitzu/bin/conn.php
@@ -1,4 +1,6 @@
 <?php
+//如果資料庫沒辦法正常登入的話，就把這行貼在workbench執行
+//ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345678';
     $user = 'root';
     $pass = '12345678';
     $db = '112504';
