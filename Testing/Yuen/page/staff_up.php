94e7413709e90537c6186104e86fc8af4337f472
LydiaTW <lydia930113@gmail.com>
2023-09-09 13:20:58 +0800

    staff

--- Testing/Yuen/page/staff_up.php
+++ Testing/Yuen/page/staff_up.php
@@ -5,7 +5,6 @@ $staff_id=$_GET['staff_id'];
 
 $staff_name = $_POST['staff_name'];
 $staff_birth = $_POST['staff_birth'];
-$staff_gender = $_POST['staff_gender'];
 $staff_tel = $_POST['staff_tel'];
 $staff_address = $_POST['staff_address'];
 $em_name = $_POST['em_name'];
@@ -15,7 +14,7 @@ $due_date = $_POST['due_date'];
 $staff_psw = $_POST['staff_psw'];
 
 $sql = "UPDATE store_staff SET staff_name = '".$staff_name."',staff_birth = '".$staff_birth."', 
-staff_gender = '".$staff_gender."',  staff_tel = '".$staff_tel."',  staff_address = '".$staff_address."', 
+staff_tel = '".$staff_tel."',  staff_address = '".$staff_address."', 
 em_name = '".$em_name."', em_tel = '".$em_tel."', relation = '".$relation."', due_date = '".$due_date."', 
 staff_psw = '".$staff_psw."' WHERE staff_id = '".$staff_id."'";
 
