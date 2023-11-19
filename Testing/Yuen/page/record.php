9246cf45f960a915b70e069534f9fadc2df1ea55
LydiaTW <lydia930113@gmail.com>
2023-11-19 17:19:41 +0800

    更新

--- Testing/Yuen/page/record.php
+++ Testing/Yuen/page/record.php
@@ -1,3 +1,10 @@
+<?php
+    include "../bin/conn.php";
+
+    $identity = $_GET["boss_identity"];
+    $store_id = $_GET["store_id"];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,10 +36,20 @@
                     </div>
                 </div>
                 <div class="twosmall">
-                    <form action="record.php" method="POST">
+                    <form  method="POST">
+                    <input type="hidden" id="boss_identity" name="boss_identity" 
+<?php      
+                //把老闆身份證號、店代號，放進隱藏欄位。供POST時使用
+                echo "value=\"$identity\">";
+?>                
+                <input type="hidden" id="store_id" name="store_id" 
+<?php                
+                //把老闆身份證號、店代號，放進隱藏欄位。供POST時使用
+                echo "value=\"$store_id\">";
+                ?> 
                         <p class="inline-form">
-                            查詢：<input type="date" name="查詢日期">
-                            <input type="submit" value="確認">
+                        搜尋：<input type="search" class="light-table-filter" data-table="order-table" placeholder="請輸入關鍵字">
+
                         </p>
                         <?php
 
@@ -46,7 +63,9 @@
                                 on a.boss_identity = b.boss_identity and a.store_id = b.store_id and a.order_no = b.order_no
                                 left join store_food as c
                                 on a.meal_id = c.meal_id
-                                where date(b.start_time) = '$查詢'"
+                                where a.boss_identity = '$identity' and a.store_id = '$store_id' and b.boss_identity = '$identity' and b.store_id = '$store_id'
+
+                               "
                                 ;
 
                             
@@ -54,7 +73,7 @@
                                 $total_records = mysqli_num_rows($result);
 
                                 
-                                echo  "<table border = '1' align = 'center'>";
+                                echo  "<table border = '1' align = 'center' class='order-table'>";
                                 echo "<tr>";
                                     echo "<th>日期</th>";
                                     echo "<th>桌號</th>";
@@ -96,12 +115,64 @@
     </div>
 </body>
 <script>
+    (function(document) {
+  'use strict';
+
+  // 建立 LightTableFilter
+  var LightTableFilter = (function(Arr) {
+
+    var _input;
+
+    // 資料輸入事件處理函數
+    function _onInputEvent(e) {
+      _input = e.target;
+      var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+      Arr.forEach.call(tables, function(table) {
+        Arr.forEach.call(table.tBodies, function(tbody) {
+          Arr.forEach.call(tbody.rows, _filter);
+        });
+      });
+    }
+
+    // 資料篩選函數，顯示包含關鍵字的列，其餘隱藏
+    function _filter(row) {
+      var text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
+      row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
+    }
+
+    return {
+      // 初始化函數
+      init: function() {
+        var inputs = document.getElementsByClassName('light-table-filter');
+        Arr.forEach.call(inputs, function(input) {
+          input.oninput = _onInputEvent;
+        });
+      }
+    };
+  })(Array.prototype);
+
+  // 網頁載入完成後，啟動 LightTableFilter
+  document.addEventListener('readystatechange', function() {
+    if (document.readyState === 'complete') {
+      LightTableFilter.init();
+    }
+  });
+
+})(document);
+
     function goBack() {
         var urlParams = new URLSearchParams(window.location.search);
         var boss_identity = urlParams.get('boss_identity');
         var store_id = urlParams.get('store_id');
         var boss_name = urlParams.get('boss_name');
         location.href="boss_management.html?boss_identity=" + boss_identity + "&store_id=" + store_id + "&boss_name=" + boss_name;
+    }
+    function goRecord() {
+        var urlParams = new URLSearchParams(window.location.search);
+        var boss_identity = urlParams.get('boss_identity');
+        var store_id = urlParams.get('store_id');
+        var boss_name = urlParams.get('boss_name');
+        location.href="record.html?boss_identity=" + boss_identity + "&store_id=" + store_id + "&boss_name=" + boss_name;
     }
 </script>
 
