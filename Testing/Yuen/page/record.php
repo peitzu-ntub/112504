e7358e8344ba8f7a85238a19ca9321e415bd9f00
LydiaTW <lydia930113@gmail.com>
2023-11-20 14:44:46 +0800

    更新

--- Testing/Yuen/page/record.php
+++ Testing/Yuen/page/record.php
@@ -1,10 +1,24 @@
 <?php
     include "../bin/conn.php";
+    
+    if (isset($_GET["boss_identity"]))
+        $identity = $_GET["boss_identity"];
+    if (isset($_GET['store_id']))
+        $store_id = $_GET["store_id"];
+    if (isset($_GET["boss_name"]))
+        $boss_name = $_GET["boss_name"];
 
-    $identity = $_GET["boss_identity"];
-    $store_id = $_GET["store_id"];
+    if (isset($_POST["boss_identity"]))
+        $identity = $_POST["boss_identity"];
+    if (isset($_POST['store_id']))
+        $store_id = $_POST["store_id"];
+    if (isset($_POST["boss_name"]))
+        $boss_name = $_POST["boss_name"];
+
+    $date_e = '';
+    if (isset($_POST['date_e']) && $_POST['date_e'] != '')
+            $date_e = $_POST['date_e'];
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,20 +34,26 @@
 </head>
 <?php
 
+
 // 設置一個空陣列來放資料
 $datas = array();
+$dateE = "";
+if (isset($_POST['date_e']) && $_POST['date_e'] != '')
+    $dateE = " and date(b.start_time) = '" . $_POST["date_e"] . "' ";
 
 $sql = "select date(b.start_time) as date, b.table_number, time(b.start_time) start_time, b.customer_count, time(b.end_time)end_time, c.meal_name
-                                FROM store_order_item as a 
-                                left join store_order as b
-                                on a.boss_identity = b.boss_identity and a.store_id = b.store_id and a.order_no = b.order_no
-                                left join store_food as c
-                                on a.meal_id = c.meal_id
-                                where a.boss_identity = '$identity' and a.store_id = '$store_id' and b.boss_identity = '$identity' and b.store_id = '$store_id'
+FROM store_order_item as a 
+left join store_order as b
+on a.boss_identity = b.boss_identity and a.store_id = b.store_id and a.order_no = b.order_no
+left join store_food as c
+on a.meal_id = c.meal_id
+where a.boss_identity = '$identity' and a.store_id = '$store_id' and b.boss_identity = '$identity' and b.store_id = '$store_id'
+$dateE
+";
 
-                               "
-                                ;
-$result = mysqli_query($con, $sql); // 用mysqli_query方法執行(sql語法)將結果存在變數中
+
+$result = mysqli_query($con, $sql);
+
 
 // 如果有資料
 if ($result) {
@@ -75,7 +95,10 @@ $datas_len = count($datas); //目前資料筆數
         </div>
     </div>
     <div class="container-wrapper">
-    <form action="record.php" method="POST">
+    <form id="main" method="POST" action="record.php" >
+                    <input type="hidden" id="boss_identity" name="boss_identity" value="<?php echo $identity; ?>" />
+                    <input type="hidden" id="store_id" name="store_id" value="<?php echo $store_id; ?>" />
+                    <input type="hidden" id="boss_name" name="boss_name" value="<?php echo $boss_name; ?>" />
             <div class="subject">
                 <div class="title">
                     <div align="left">
@@ -84,35 +107,32 @@ $datas_len = count($datas); //目前資料筆數
                     </div>
                 </div>
                 <div class="twosmall">
-                    <form  method="POST">
-                    <input type="hidden" id="boss_identity" name="boss_identity" 
-<?php      
-                //把老闆身份證號、店代號，放進隱藏欄位。供POST時使用
-                echo "value=\"$identity\">";
-?>                
-                <input type="hidden" id="store_id" name="store_id" 
-<?php                
-                //把老闆身份證號、店代號，放進隱藏欄位。供POST時使用
-                echo "value=\"$store_id\">";
-                ?> 
+                    
                         <p class="inline-form">
-                            查詢：<input type="search" name="查詢日期" class="light-table-filter" data-table="order-table" placeholder="請輸入日期">
-                            <input type="submit" value="確認" >
+                            查詢：
+                            <?php 
+    if ($date_e != '') 
+        echo "
+                        <input type=\"date\" name=\"date_e\" style=\"font-size: 20px;\" value=\"$date_e\">";
+    else echo "
+                        <input type=\"date\" name=\"date_e\" style=\"font-size: 20px;\">";
+?> 
+                            <input type="submit" value="確認">
                         </p>
-                        <div class="ininsidebox" style="width:700px;height:330px; overflow:auto;">
-                        <table border = '1' align = 'center' class='order-table'>
-                    <thead>    
-                    <tr>
-                       <th>日期</th>
-                                    <th>桌號</th>
-                                    <th>開桌時間</th>
-                                    <th>人數</th>
-                                    <th>關桌時間</th>
-                                    <th>餐點</th>
-                        </tr>
-                    </thead>
-                        <tbody>
-                            <?php
+                        <div class="ininsidebox" style="width:900px;height:330px; overflow:auto;">
+                            <table border='1' align='center' class='order-table'>
+                                <thead>
+                                    <tr>
+                                        <th>日期</th>
+                                        <th>桌號</th>
+                                        <th>開桌時間</th>
+                                        <th>人數</th>
+                                        <th>關桌時間</th>
+                                        <th>餐點</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                             <?php
                             for ($i = 0; $i < $datas_len; $i++) {
                                 echo "<tr>";
                                 echo "<td>". $datas[$i]['date'] . "</span>";
@@ -124,78 +144,19 @@ $datas_len = count($datas); //目前資料筆數
                                 echo "</br>";
                             }
                             ?>
-
-                            </tbody>  
-                    </table>
-
-                </div>
-                        
-                    </form>
-
-                    
-            </div>
+                                </tbody>
+                            </table>
+                        </div>
         </form>
     </div>
 </body>
 <script>
-    (function(document) {
-  'use strict';
-
-  // 建立 LightTableFilter
-  var LightTableFilter = (function(Arr) {
-
-    var _input;
-
-    // 資料輸入事件處理函數
-    function _onInputEvent(e) {
-      _input = e.target;
-      var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
-      Arr.forEach.call(tables, function(table) {
-        Arr.forEach.call(table.tBodies, function(tbody) {
-          Arr.forEach.call(tbody.rows, _filter);
-        });
-      });
-    }
-
-    // 資料篩選函數，顯示包含關鍵字的列，其餘隱藏
-    function _filter(row) {
-      var text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
-      row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
-    }
-
-    return {
-      // 初始化函數
-      init: function() {
-        var inputs = document.getElementsByClassName('light-table-filter');
-        Arr.forEach.call(inputs, function(input) {
-          input.oninput = _onInputEvent;
-        });
-      }
-    };
-  })(Array.prototype);
-
-  // 網頁載入完成後，啟動 LightTableFilter
-  document.addEventListener('readystatechange', function() {
-    if (document.readyState === 'complete') {
-      LightTableFilter.init();
-    }
-  });
-
-})(document);
-
-    function goBack() {
+     function goBack() {
         var urlParams = new URLSearchParams(window.location.search);
-        var boss_identity = urlParams.get('boss_identity');
-        var store_id = urlParams.get('store_id');
-        var boss_name = urlParams.get('boss_name');
+        var boss_identity = '<?php echo $identity; ?>'; 
+        var store_id = '<?php echo $store_id; ?>';
+        var boss_name = '<?php echo $boss_name; ?>';
         location.href="boss_management.html?boss_identity=" + boss_identity + "&store_id=" + store_id + "&boss_name=" + boss_name;
-    }
-    function goRecord() {
-        var urlParams = new URLSearchParams(window.location.search);
-        var boss_identity = urlParams.get('boss_identity');
-        var store_id = urlParams.get('store_id');
-        var boss_name = urlParams.get('boss_name');
-        location.href="record.html?boss_identity=" + boss_identity + "&store_id=" + store_id + "&boss_name=" + boss_name;
     }
 </script>
 
